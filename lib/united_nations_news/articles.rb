20b2320b4706d6fb9ee538d83cfb825c87fdff4a
Mic Fox <michael.c.fox.ii@gmail.com>
2019-07-14 22:19:22 -0400

    initialize article headlines in Articles class

--- lib/united_nations_news/articles.rb
+++ lib/united_nations_news/articles.rb
@@ -1,4 +1,16 @@
 class UnitedNationsNews::Articles
+    attr_accessor :name, :url
 
-    
+    @@all = []
+
+    def initialize(name = nil, url = nil)
+        @name = name
+        @url = "https://news.un.org/en/news" + url
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
