5bafa044a85428aceccde4134124cf4db577dc44
Mic Fox <michael.c.fox.ii@gmail.com>
2019-07-24 14:02:37 -0400

    added loading element from previous project

--- lib/united_nations_news/cli.rb
+++ lib/united_nations_news/cli.rb
@@ -1,4 +1,26 @@
 class UnitedNationsNews::CLI
 
+    def call
+    # loading
+    
+    end
 
+
+
+    def loading 
+        spinner = Enumerator.new do |e|
+            loop do
+                e.yield '|'
+                e.yield '/'
+                e.yield '-'
+                e.yield '\\'
+            end
+        end
+
+        1.upto(100) do |i|
+            progress = "=" * (i/5) unless i < 5
+            printf("\rRetrieving United Nations Top News Stories: [%-20s] %d%% %s", progress, i , spinner.next)
+            sleep(0.1)
+        end
+    end
 end
